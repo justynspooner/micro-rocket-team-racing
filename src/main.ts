d71891efb6636b73ed4c251ce1f9b4ff591fea6d
Justyn Spooner <justyn@dovu.io>
2024-02-20 22:48:39 +0000

    Remove injected HTML

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,6 @@ import { init } from "./lib/hashgraph.ts";
 
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Micro Rocket Team Racing</h1>
-    <p class="read-the-docs">
-      Connect to log your lap times and see the leaderboard.
-    </p>
-    <canvas id="canvas" width="700" height="700" style="background-color: #3F8EF0;"></canvas>
-  </div>
-`;
-
 document.addEventListener("DOMContentLoaded", () => {
   init();
   new Game().start();
